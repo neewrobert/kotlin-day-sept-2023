2ac16de8bacdbb107fd4b7ed055ca8b7944c937a
Newton dos Santos <neewrobert@gmail.com>
2023-09-22 09:37:50 +0200

    Add routes

--- ktor-fundamentals-start/src/main/kotlin/org/fundamentals/plugins/Routing.kt
+++ ktor-fundamentals-start/src/main/kotlin/org/fundamentals/plugins/Routing.kt
@@ -3,8 +3,11 @@ package org.fundamentals.plugins
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.fundamentals.model.Priority
+import org.fundamentals.model.Task
 import org.fundamentals.model.TaskRepository
 import org.fundamentals.view.tasksAsTable
 
@@ -36,6 +39,50 @@ fun Application.configureRouting() {
                     text = listOf(task).tasksAsTable()
                 )
             }
+
+            get("/byPriority/{priority}") {
+
+                val priority = call.parameters["priority"]
+                if (priority == null || priority !in Priority.entries.map { it.name }) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@get
+                }
+                val tasks = TaskRepository.tasksByPriority(Priority.valueOf(priority))
+                call.respondText(
+                    contentType = ContentType.parse("text/html"),
+                    text = tasks.tasksAsTable()
+                )
+            }
+
+            post {
+                val formContent = call.receiveParameters()
+                val params = Triple(
+                    formContent["name"] ?: "",
+                    formContent["description"] ?: "",
+                    formContent["priority"] ?: ""
+                )
+                if (params.toList().any { it.isEmpty() }) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@post
+                }
+                try {
+                    val priority = Priority.valueOf(params.third)
+                    TaskRepository.addTask(
+                        Task(
+                            params.first,
+                            params.second,
+                            priority
+                        )
+                    )
+                    call.respondRedirect("/tasks")
+                } catch (ex: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                } catch (ex: IllegalStateException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+            }
         }
+
+
     }
 }
